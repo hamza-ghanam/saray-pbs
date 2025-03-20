d2333bf1f97fb4013ad2881725d90a9c36bc8fff
Hamza Ghanam <hamza.ghanam.92@gmail.com>
2025-03-20 14:41:22 +0400

    Updating the ENV file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 use OpenApi\Attributes as OA;
 
 #[
-    OA\Info(title: "Property Booking System", version: "1.0"),
-    OA\server(url: 'http://127.0.0.1:8000/api', description: 'IP'),
+    OA\Info(title: "Saray Property Booking System", version: "1.0"),
     OA\server(url: 'http://localhost:8000/api', description: 'URL'),
     OA\securityScheme(securityScheme: 'bearerAuth', type: 'http', name: 'Authorization', in: 'header', scheme: 'bearer'),
 ]
