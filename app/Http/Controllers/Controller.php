e944ddead68e41166f10d5ec758334fd711d1c57
Hamza Ghanam <hamza.ghanam.92@gmail.com>
2025-04-25 14:16:24 +0400

    Start with version 2.0

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use OpenApi\Attributes as OA;
 
 #[
-    OA\Info(title: "Saray Property Booking System", version: "1.0"),
+    OA\Info(title: "Saray Property Booking System", version: "2.0"),
     OA\server(url: 'http://localhost:8000/api', description: 'URL'),
     OA\securityScheme(securityScheme: 'bearerAuth', type: 'http', name: 'Authorization', in: 'header', scheme: 'bearer'),
 ]
