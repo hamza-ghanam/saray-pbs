c0e673ada0afdb3826067090351c0270487b1b58
Hamza Ghanam <hamza.ghanam.92@gmail.com>
2026-03-28 16:45:54 +0400

    Fix translate multiple texts. Error 429

--- app/Services/TranslationService.php
+++ app/Services/TranslationService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use Stichoza\GoogleTranslate\Exceptions\LargeTextException;
+use Stichoza\GoogleTranslate\Exceptions\RateLimitException;
+use Stichoza\GoogleTranslate\Exceptions\TranslationRequestException;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class TranslationService
@@ -19,15 +22,36 @@ class TranslationService
         return $tr->translate($text);
     }
 
+    /**
+     * @throws LargeTextException
+     * @throws RateLimitException
+     * @throws TranslationRequestException
+     */
     public function translateMultiple(array $texts): array
     {
         $tr = new GoogleTranslate($this->target);
         $tr->setSource($this->source);
 
+        // Use a unique delimiter unlikely to appear in normal text
+        $delimiter = "|||__||__|||";
+
+        // Join all texts into one string
+        $joined = implode($delimiter, $texts);
+
+        // Single API call
+        $translated = $tr->translate($joined);
+
+        // Split back into array
+        $parts = explode($delimiter, $translated);
+
+        // Re-map to original keys
         $out = [];
-        foreach ($texts as $k => $t) {
-            $out[$k] = $tr->translate($t);
+        $keys = array_keys($texts);
+
+        foreach ($keys as $i => $key) {
+            $out[$key] = $parts[$i] ?? null;
         }
+
         return $out;
     }
 }
