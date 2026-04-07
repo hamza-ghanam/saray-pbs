e170ae96ee7eb961b26b3109e47f1f09b8397749
Hamza Ghanam <hamza.ghanam.92@gmail.com>
2026-04-07 20:50:20 +0400

    Fix middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,8 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectGuestsTo(fn() => null);
         $middleware->alias([
             'swagger.auth' => SwaggerAuth::class,
-        ]);
-        $middleware->alias([
             'permission' => PermissionMiddleware::class,
             'role' => RoleMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
