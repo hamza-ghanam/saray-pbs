0e5616dad776fdb65ccc0b171c9de916e515773c
Hamza Ghanam <hamza.ghanam.92@gmail.com>
2026-06-09 10:56:59 +0400

    Fix HandleCors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
+
         $middleware->redirectGuestsTo(fn() => null);
         $middleware->alias([
             'swagger.auth' => SwaggerAuth::class,
